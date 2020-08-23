f90e152e87e3a3105e39c6a92294d079ed555902
Abdulrahman Hashem <abd.alrahmanh.1995@gmail.com>
2020-08-23 23:24:31 +0300

    - Update readme.md.

--- src/Handler/BaseEmailHandler.php
+++ src/Handler/BaseEmailHandler.php
@@ -12,12 +12,12 @@ class BaseEmailHandler
         // Instantiation and passing `true` enables exceptions
         $mail = new PHPMailer(true);
         // Server settings
-        $mail->SMTPDebug = SMTP::DEBUG_SERVER;                     // Enable verbose debug output
-        $mail->isSMTP();                                           // Send using SMTP
-        $mail->Host       = 'smtp.gmail.com';                    // Set the SMTP server to send through
-        $mail->SMTPAuth   = true;                                  // Enable SMTP authentication
-        $mail->Username   = 'kobybryant.h@gmail.com';              // SMTP username
-        $mail->Password   = 'kbso83295';                           // SMTP password
+        $mail->SMTPDebug = SMTP::DEBUG_SERVER;                      // Enable verbose debug output
+        $mail->isSMTP();                                            // Send using SMTP
+        $mail->Host       = 'smtp.gmail.com';                       // Set the SMTP server to send through
+        $mail->SMTPAuth   = true;                                   // Enable SMTP authentication
+        $mail->Username   = '';                                     // TODO: Put your SMTP username
+        $mail->Password   = '';                                     // TODO : Put your SMTP password
         $mail->SMTPSecure = PHPMailer::ENCRYPTION_STARTTLS;        // Enable TLS encryption; `PHPMailer::ENCRYPTION_SMTPS` encouraged
         $mail->Port       = 587;                                   // TCP port to connect to, use 465 for `PHPMailer::ENCRYPTION_SMTPS` above
         return $mail;
